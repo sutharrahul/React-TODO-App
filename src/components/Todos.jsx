d44cad44617f5cdad92cc7d4fa7cbf98d0fc293f
Rahul suthar <sutharrahul119@gmail.com>
2024-03-10 19:02:35 +0530

    text overflow bug resolve :fire:

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -48,65 +48,68 @@ function Todos() {
           Add
         </button>
       </div>
+
       <div>
-        <div>
-          {todoList.map((todo) => (
-            <div className="flex justify-between mt-[5px] mb-[5px] border-2 border-stone-700 rounded px-[5px] py-[10px]">
-              <span className="flex items-center text-white text-[1.5rem] px-[5px]">
-                {todo.text}
-              </span>
-              <div className="flex justify-between">
-                {editId != todo.id && (
-                  <span>
-                    <button
-                      onClick={() => {
-                        setTodoValue(todo.text);
-                        setEditId(todo.id);
-                      }}
-                      className="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white mr-1"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                        />
-                      </svg>
-                      Edit
-                    </button>
-                  </span>
-                )}
-                <button
-                  className="flex p-2.5 bg-red-600 rounded-xl hover:rounded-3xl hover:bg-red-700 transition-all duration-300 text-white"
-                  onClick={() => deleteTodo(todo.id)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+        {todoList?.map((todo, i) => (
+          <div
+            key={i}
+            className="flex justify-between mt-[5px] mb-[5px] border-2 border-stone-700 rounded px-[5px] py-[10px] truncate w-full "
+          >
+            <span className="flex overflow-auto items-center text-wrap text-white text-lg px-[5px]  ">
+              {todo.text}
+            </span>
+
+            <div className="flex justify-between">
+              {editId != todo.id && (
+                <span>
+                  <button
+                    onClick={() => {
+                      setTodoValue(todo.text);
+                      setEditId(todo.id);
+                    }}
+                    className="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white mr-1"
                   >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    ></path>
-                  </svg>
-                  Delete
-                </button>
-              </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                      />
+                    </svg>
+                    Edit
+                  </button>
+                </span>
+              )}
+              <button
+                className="flex p-2.5 bg-red-600 rounded-xl hover:rounded-3xl hover:bg-red-700 transition-all duration-300 text-white"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  ></path>
+                </svg>
+                Delete
+              </button>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
